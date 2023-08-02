c193a95b52890e89e5ff47e521a6d5817a37a126
Daniel Barillas <98109214+DanielBarillas@users.noreply.github.com>
2023-08-02 16:29:36 -0600

    Versión final por Daniel Barillas, Carné No. 22193
    Versión final del programa del laboratorio 3, por: Pablo Daniel Barillas Moreno, Carné No. 22193

---
+++ src/main/java/com/example/laboratorio3/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.laboratorio3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.laboratorio3.ui.theme.Laboratorio3Theme
+import com.example.laboratorio3.ui.theme.Purple80
+import androidx.compose.ui.graphics.Color
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Laboratorio3Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Daniel Barillas, Carné No.22193")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier, ) {
+    Surface(color = Color.Blue) {
+        Text(
+            text = "Hola, mi nombre es: $name!", color = Purple80, fontSize = 24.sp,
+            modifier = modifier
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio3Theme {
+        Greeting("Android")
+    }
+}
